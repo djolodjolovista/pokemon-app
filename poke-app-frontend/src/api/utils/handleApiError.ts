486313dfded1f2daf8324d9ef0afced67970cb2a
Djordje <tkd_djole@rocketmail.com>
2026-01-11 21:08:43 +0100

    refactor: improve error handling in handleApiError function

--- poke-app-frontend/src/api/utils/handleApiError.ts
+++ poke-app-frontend/src/api/utils/handleApiError.ts
@@ -1,14 +1,20 @@
-import type { AxiosError } from 'axios'
+import { isAxiosError, type AxiosError } from 'axios'
 import { toast } from 'react-hot-toast'
 
 export const handleApiError = (error: unknown) => {
   const axiosError = error as AxiosError<{ message?: string }>
+  if (isAxiosError(error)) {
+    const message = axiosError?.response?.data?.message || axiosError.message || 'Request failed!'
 
-  const message =
-    axiosError?.response?.data?.message ||
-    (error instanceof Error ? error.message : 'Unexpected error')
+    toast.error(message)
 
-  toast.error(message)
-
-  return message
+    return message
+  }
+  if (error instanceof Error) {
+    toast.error(error.message)
+    return error.message
+  }
+  const fallbackMessage = 'An unknown error occurred'
+  toast.error(fallbackMessage)
+  return fallbackMessage
 }
